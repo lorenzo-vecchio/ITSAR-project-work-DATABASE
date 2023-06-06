7228f67e2e407421d2639fdc6856ba78c012db79
Riccardo.Frigerio <134052922+Riky000000003@users.noreply.github.com>
2023-06-06 12:27:47 +0200

    .

--- test.sql
+++ test.sql
@@ -8,3 +8,6 @@ INSERT INTO razze (nomeRazza, id_specie) VALUES ('Canarino', 3);
 INSERT INTO animali (nomeAnimale, sesso, data_di_nascita, id_razza, id_specie, id_utente) VALUES ('Fido', 'M', '2020-01-01', 1, 1, 1);
 INSERT INTO animali (nomeAnimale, sesso, data_di_nascita, id_razza, id_specie, id_utente) VALUES ('Whiskers', 'F', '2019-05-15', 2, 2, 1);
 INSERT INTO animali (nomeAnimale, sesso, data_di_nascita, id_razza, id_specie, id_utente) VALUES ('Tweety', 'M', '2022-03-10', 3, 3, 1);
+
+INSERT INTO promemoria (titolo, descrizione, data_e_ora, id_animale, id_utente)
+VALUES ("veterinaio", "Portare fido dal veterinario", '2023-06-20', 1, 1);
