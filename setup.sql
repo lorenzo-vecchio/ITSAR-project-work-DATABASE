dee13070e0fb40afcec7f6c8a3ec90736b97faa0
Riccardo.Frigerio <134052922+Riky000000003@users.noreply.github.com>
2023-06-09 15:37:02 +0200

    .

--- setup.sql
+++ setup.sql
@@ -1,6 +1,5 @@
 insert into localita (nomeLocalita, provincia, regione)
 values ("Milano", "Milano", "Lombardia"),
-("Milano", "Milano", "Lombardia"),
 ("Roma", "Roma", "Lazio"),
 ("Napoli", "Napoli", "Campania");
 
