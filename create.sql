fa8b4a55f138c5860324063d13dfed9ca847569d
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2023-07-04 11:25:33 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,93 +1,100 @@
-use railway;
+CREATE TABLE `animali` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomeAnimale` varchar(255) NOT NULL,
+  `sesso` char(1) NOT NULL,
+  `data_di_nascita` date NOT NULL,
+  `immagine` blob,
+  `id_razza` int DEFAULT NULL,
+  `id_utente` int DEFAULT NULL,
+  `peso` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_razza` (`id_razza`),
+  KEY `id_utente` (`id_utente`),
+  CONSTRAINT `animali_ibfk_1` FOREIGN KEY (`id_razza`) REFERENCES `razze` (`id`),
+  CONSTRAINT `animali_ibfk_2` FOREIGN KEY (`id_utente`) REFERENCES `utenti` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table utenti
-(
-    id int primary key auto_increment,
-    userName varchar(255) not null,
-    password varchar(255) not null,
-    email varchar(255) not null,
-    immagine blob
-);
+CREATE TABLE `localita` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomeLocalita` varchar(255) NOT NULL,
+  `provincia` varchar(200) NOT NULL,
+  `regione` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table specie
-(
-    id int primary key auto_increment,
-    nomeSpecie varchar(255) not null
-);
+CREATE TABLE `preferiti` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_utente` int DEFAULT NULL,
+  `id_servizi` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_utente` (`id_utente`),
+  KEY `id_servizi` (`id_servizi`),
+  CONSTRAINT `preferiti_ibfk_1` FOREIGN KEY (`id_utente`) REFERENCES `utenti` (`id`),
+  CONSTRAINT `preferiti_ibfk_2` FOREIGN KEY (`id_servizi`) REFERENCES `servizi` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table razze 
-(
-    id int primary key auto_increment,
-    nomeRazza varchar(255) not null,
-    id_specie int,
-    foreign key (id_specie) references specie (id)
-);
+CREATE TABLE `promemoria` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titolo` varchar(255) DEFAULT NULL,
+  `descrizione` varchar(500) DEFAULT NULL,
+  `data_e_ora` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table animali 
-(
-    id int primary key auto_increment,
-    nomeAnimale varchar(255) not null,
-    sesso char(1) not null,
-    data_di_nascita date not null,
-    peso float,
-    immagine blob,
-    id_razza int,
-    id_utente int,
-    foreign key (id_razza) references razze (id),
-    foreign key (id_utente) references utenti (id)
-);
+CREATE TABLE `razze` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomeRazza` varchar(255) NOT NULL,
+  `id_specie` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_specie` (`id_specie`),
+  CONSTRAINT `razze_ibfk_1` FOREIGN KEY (`id_specie`) REFERENCES `specie` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-alter table animali add peso float;
+CREATE TABLE `riferimento` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_promemoria` int DEFAULT NULL,
+  `id_animale` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_animale` (`id_animale`),
+  KEY `id_promemoria` (`id_promemoria`),
+  CONSTRAINT `riferimento_ibfk_1` FOREIGN KEY (`id_animale`) REFERENCES `animali` (`id`),
+  CONSTRAINT `riferimento_ibfk_2` FOREIGN KEY (`id_promemoria`) REFERENCES `promemoria` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table localita
-(
-    id int primary key auto_increment,
-    nomeLocalita varchar(255) not null,
-    provincia varchar(200) not null,
-    regione varchar(100) not null
-);
+CREATE TABLE `servizi` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomeLuogo` varchar(255) NOT NULL,
+  `latitudine` decimal(9,6) NOT NULL,
+  `longitudine` decimal(9,6) NOT NULL,
+  `id_tipo_servizio` int DEFAULT NULL,
+  `id_localita` int DEFAULT NULL,
+  `approvato` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_tipo_servizio` (`id_tipo_servizio`),
+  KEY `id_localita` (`id_localita`),
+  CONSTRAINT `servizi_ibfk_1` FOREIGN KEY (`id_tipo_servizio`) REFERENCES `tipologia_servizi` (`id`),
+  CONSTRAINT `servizi_ibfk_2` FOREIGN KEY (`id_localita`) REFERENCES `localita` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table tipologia_servizi
-(
-    id int primary key auto_increment,
-    nomeTipo varchar(255) not null
-);
+CREATE TABLE `specie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomeSpecie` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table servizi
-(
-    id int primary key auto_increment,
-    nomeLuogo varchar(255) not null,
-    latitudine int not null,
-    longitudine int not null,
-    id_tipo_servizio int,
-    id_localita int,
-    approvato bit,
-    foreign key (id_tipo_servizio) references tipologia_servizi(id),
-    foreign key (id_localita) references localita(id)
-);
+CREATE TABLE `tipologia_servizi` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomeTipo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table preferiti
-(
-    id int primary key auto_increment,
-    id_utente int,
-    id_servizi int,
-    foreign key (id_utente) references utenti(id),
-    foreign key (id_servizi) references servizi(id)
-);
-
-create table promemoria
-(
-    id int primary key auto_increment,
-    titolo varchar(255),
-    descrizione varchar(500),
-    data_e_ora datetime
-);
-
-create table riferimento
-(
-	id int auto_increment primary key, 
-    id_promemoria int,
-    id_animale int,
-	foreign key (id_animale) references animali(id),
-    foreign key (id_promemoria) references promemoria (id)
-); 
+CREATE TABLE `utenti` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userName` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `cognome` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `immagine` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
